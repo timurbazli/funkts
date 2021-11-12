dae71e44d1934b44c0189e3345f79f129c6ac0d1
opilane <opilane@HP-CZC6297BB4>
2021-11-12 13:12:58 +0200

    tet34636364346

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -2,7 +2,7 @@ from module1 import *
 
 while 1:
     print("Funktsioonid".center(50," "))
-    v=input("Arithmetic - A, Is_year_leap - Y, Square - S, Season - M")
+    v=input("Arithmetic - A\nIs_year_leap - Y\nSquare - S\nSeason - M\nBank - B\nIs_prime - I\nXor_cipher - X\nDate - D")
     v=input()
 
     if v.upper()=="A":
@@ -15,8 +15,30 @@ while 1:
         rezult=is_year_leap(int(input("Sisesta aasta: ")))
         print(rezult)
     if v.upper()=="S":
-        rezult=square(int(input("Sisesta ruudu külk: ")))
+        sqr=(float(input("Sisesta ruudu külk: ")))
+        rezult=square(sqr)
         print(rezult)
     if v.upper()=="M":
         rezult=season(int(input("Sisesta kuu: ")))
+        print(rezult)
+    if v.upper()=="B":
+        summa=(float(input("Sisesta raha: ")))
+        years=(int(input("Sisesta aasta: ")))
+        rezult=bank(summa, years)
+        print(f"Вы получите {rezult} за {years} years.")
+    if v.upper()=="I":
+        rezult=is_prime(float(input("Sisesta number: ")))
+        print(rezult)
+    if v.upper()=="X":
+        print("Kodeerimine".center(60,"*"))
+        rezult=xor_cipher(input("Sisesta text: "), input("Sisesta võti: "))
+        print(rezult)
+        print("Dekodeerimine".center(60,"*"))
+        de_rezult=xor_cipher(rezult, input("Sisesta võti: "))
+        print(de_rezult)
+    if v.upper()=="D":
+        day=int(input("Sisesta päev:"))
+        month=int(input("Sisesta month:"))
+        year=int(input("Sisesta year:"))
+        rezult=date(day, month, year)
         print(rezult)
--- module1.py
+++ module1.py
@@ -1,4 +1,5 @@
 from math import *
+from random import *
 
 def arithmetic(a: float, b: float, c:str):
     """Lihtne kalkulaator
@@ -10,6 +11,7 @@ def arithmetic(a: float, b: float, c:str):
     :param float b: Teine arv
     :param str c: Aritmeetiline tehnig
     :rtype float:
+    :rtype var: Märamata tüüb
     """
     if c=="+":
         r=a+b
@@ -21,11 +23,9 @@ def arithmetic(a: float, b: float, c:str):
         if b!=0:
             r=a/b
         else:
-            print("Div0")
-#            r=var 
-            r=0.0
+            r="Div/0"
     else:
-        print("Viga")
+        r="Tundmatu sym"
         r=0.0
     return r
 
@@ -34,6 +34,7 @@ def is_year_leap(year: int):
     Tagastab true kui aasta on liigaasta ja False kui ei ole
     :param int aasta: Aasta number
     :rtype bool: Funktsioni vastus logilises formaadis
+    Kirjuame suvalise aasta ja programm määrab ära, kas viisaasta või liifaasta
     """
     if year%4==0:
         vastus=True
@@ -42,40 +43,86 @@ def is_year_leap(year: int):
     return vastus
 
 def square(s1:float):
+    """Программа принимает 1 сторону квадрата, при нахождении площади (S=a*a,), при нахождении пириметра (P=a*4), а диагональ мы находим при помощи теоремы Пифагора.
+    otv - переменная, которую я сделал для return
+    """
     if s1!=0:
-        otv=""
+#        otv=""
         p=s1*4
         s=s1*s1
         d=sqrt(s1*s1+s1*s1)
-        print("P=", p)
-        print("S=", s)
-        print("d=", d)
-    return otv
+        r=(p,s,d)
+
+#        print("P=", p)
+#        print("S=", s)
+#        print("d=", d)
+    return r
 
 def season(kuu:int):
 
     """
-    ord - десятичное число, которое кодируется в букву. 
-    chr - наборот берёт число и возвращает букву.
+Программа принимает 
+:param int kuu: kuu jarjekordne number
+:rtype str: hooja nimetus
     """
-    if kuu== 12 or 1 <= kuu <=2:
+    if kuu== 12 or 1 <= kuu <=2:        #Если месяц 12,1,2 - то это зима
         vd="See on talv"
-    elif 3 <= kuu <= 5:
+    elif 3 <= kuu <= 5:        #Если месяц 3,4,5 - то это весна
         vd="See on kevad"
-    elif 6 <= kuu <= 8:
+    elif 6 <= kuu <= 8:        #Если месяц 6,7,8 - то это лето
         vd="See on suvi"
-    elif 9 <= kuu <= 11:
+    elif 9 <= kuu <= 11:        #Если месяц 9,10,11 - то это осень
         vd="See on sügi"
     else:
-        vd="Viga!"
+        vd="Viga!"              #Если это любое другое число - то это ошибка.
     return vd
 
-#def bank(ban:float):
-#    if ban==0:
-#        total="Viga!"
-#    elif ban*1.1:
+def is_prime(numb:float):
+    if numb==0:         #Если число 0, то ошибка
+        vdl="Viga"
+    elif numb%numb==0:     #Если число делится само на себя без остатка, то True, если нет, то False
+        vdl=True
+    else:               
+        vdl=False
+    return vdl
+
+def xor_cipher(string:str, key:str)->str:
+    rezult=""
+    temp=int()
+    for i in range(len(string)):
+        temp=ord(string[i]) #näitab sümboli kood
+        for j in range(len(key)):
+            temp^=ord(key[j])       #ord - десятичное число, которое кодируется в букву. 
+        rezult +=chr(temp)
+    return rezult
 
 
+def  XOR_uncipher(string:str, key:str)->str:
+    """Tavaline sõna kodeeritakse
+    """
+    rezult=""
+    temp= []
+    for i in range (len(string)):
+        temp.append(string[i]) # Näitab kood 
+        for j in reversed(range(len(key))):
+            temp[i] = chr(ord(key[j])^ ord(temp[i]))    #chr - наоборот берёт число и возвращает букву.
+        rezult+=temp[i]
+    return rezult
 
-#    ord - десятичное число, которое кодируется в букву. 
-#    chr - наборот берёт число и возвращает букву.
+def bank(summa:float, years:int):
+    if summa>=0:
+        procenti=years*1.1
+        proc=summa*procenti
+    return proc
+
+def date(day:int, month: int, year: int):
+    if year%4==0 and day ==29:
+        if ((month>=1) and (month<=12)) and (day<=31 or day==31) and year >0:
+                otvet=True
+    else: 
+        otvet=False
+    return otvet
+        
+
+
+#:rtype bool: - значение или True или False
